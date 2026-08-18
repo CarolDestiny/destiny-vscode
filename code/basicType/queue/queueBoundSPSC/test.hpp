767d36b9c472ccac72a83afb5bcf34c45e1bdeb1
CarolDestiny <2306819382@qq.com>
2026-08-18 18:29:05 +0800

    optimize the test of queueBoundSPSC

--- code/basicType/queue/queueBoundSPSC/test.hpp
+++ code/basicType/queue/queueBoundSPSC/test.hpp
@@ -12,50 +12,35 @@ void destiny::basicType::queue::queueBoundSPSC::bench() noexcept
 {
 	using namespace std::chrono;
 
-	constexpr size_t producerNumber = 1;
-	constexpr size_t consumerNumber = 1;
 	constexpr size_t len = 100'000'000;
 	destiny::QueueBoundSPSC<void*> queue;
 
-	std::thread thProducer[producerNumber];
-	std::thread thConsumer[consumerNumber];
+	std::thread thProducer;
+	std::thread thConsumer;
 
 	const auto start = high_resolution_clock::now();
 
-	for (auto& value : thProducer) {
-		value = std::thread([&] {
-			for (size_t j = 1; j < len; j++) {
-				queue.push(reinterpret_cast<void*>(j));
-			}
-			return;
-		});
-	}
+	thProducer = std::thread([&] {
+		for (size_t i = 1; i < len; ++i)
+			queue.push((void*)(i));
+	});
 
-	for (auto& value : thConsumer) {
-		value = std::thread([&] {
-			void* a;
-			for (size_t j = 1; j < len; j++) {
-				queue.pop(a);
-			}
-			return;
-		});
-	}
+	thConsumer = std::thread([&] {
+		void* a = nullptr;
+		while (true) {
+			queue.pop(a);
+			if ((size_t)a == len - 1)
+				break;
+		}
+	});
 
-	for (auto& value : thProducer) {
-		value.join();
-	}
-
-	for (auto& value : thConsumer) {
-		value.join();
-	}
+	thProducer.join();
+	thConsumer.join();
 
 	const auto end = high_resolution_clock::now();
 	const auto duration = duration_cast<microseconds>(end - start);
 
 	std::cout << "bench: " << "destiny::basicType::queue::queueBoundSPSC\n";
 	std::cout << "\tsize:auto(256)\n";
-	std::cout << "\tproducerNumber:" << producerNumber << '\n';
-	std::cout << "\tconsumerNumber:" << consumerNumber << '\n';
-	std::cout << "\tspend:" << (std::uint64_t)len * producerNumber * 1000 * 1000 / (size_t)duration.count() << "msg/s"
-	          << std::endl;
+	std::cout << "\tspend:" << (std::uint64_t)len * 1000 * 1000 / (size_t)duration.count() << "msg/s" << std::endl;
 }
